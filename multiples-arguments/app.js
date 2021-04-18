4f56e27c5feb91d364b3718a635d5953e7f0fd86
Philipe Sanogo <philippe.sanogo@gmail.com>
2021-04-18 19:09:23 +0200

    ajout de code dans app.js

---
+++ multiples-arguments/app.js
@@ -0,0 +1,6 @@
+const sayHello = (firstName, jobExpectation) => {
+    console.log(`Bonjour, je m'appelle ${firstName}.`);
+    console.log(`Je suis ${jobExpectation}.`);
+}
+
+sayHello("Philippe", "Développeur Web");
